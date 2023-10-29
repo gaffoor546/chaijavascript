6ac178ef3b6abe125ac70a70885141c3b41336b6
Abdul Gaffoor <gaffoor546@gmail.com>
2023-10-29 11:44:16 +0000

    container config done

---
+++ basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("Hello") alert(3+9) we are using node js, not browser
+
+
+console.log(3
+     + 
+     9) // code readibillity should be high
+
+console.log("Gaffoor")
+
+let name = "Gaffoor" // string
+let age = 18 // number
+let isloggedIn = true
+let state; // undefine
+let country =  //null
+
+
+
+
+// number ==> 2 to power 53
+// bigint ==>
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value  (which is datatype of object)
+// undefine ==> 
+// symbol ==> symbol define unique 
+
+// object 
+
+console.log(typeof undefined);
