b0ea3690aee46e69ea1cdeed32ad24e65ed0a69e
Abdul Gaffoor <gaffoor546@gmail.com>
2023-11-05 21:26:32 +0530

    Create basics_part2.js

---
+++ basics_part2.js
@@ -0,0 +1,43 @@
+// array
+
+const myArray = [1,2,3,4,5,6,7,8]
+// console.log(myArray);
+
+const myHeros = ["IronMan", "Thor", "SpiderMan"]
+// console.log(myHeros);
+// console.log(myHeros[2]);
+// console.log(myHeros.length);
+
+// myHeros.push("blackpanther")
+
+// console.log(myHeros[3]);
+
+// myHeros.pop()
+
+// console.log(myHeros);
+
+// myHeros.unshift(0,9)
+// myHeros.unshift(10)
+// console.log(myHeros);
+// myHeros.shift()
+
+// console.log(myHeros.includes(9));
+//console.log(myHeros.indexOf(9));
+// const newHeros = myHeros.join()
+// console.log(myHeros);
+// console.log(newHeros);
+
+//*********** */ Slice Splice
+
+console.log("A", myArray)
+
+const myn1 = myArray.slice(1,3)
+
+console.log(myn1);
+
+console.log("B", myArray)
+
+
+const myn2 = myArray.splice(1,3)
+console.log("C", myArray)
+console.log(myn2);
